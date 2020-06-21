7d126992f068d139030cd022019defb3c6a9f88f
AhmedMostafa1212 <ahmedmostafaa1298@gmail.com>
2020-06-21 21:31:26 +0200

    RandaMagdy

---
+++ 2mrad.js
@@ -0,0 +1,40 @@
+
+   
+$("#PLUSEBTN").click(function() {
+  
+    $("#Brief").toggle(500)
+})
+
+
+//
+$("#plus").click(function() {
+  
+    $("#description-1").toggle(500)
+})
+//
+$("#plus2").click(function() {
+  
+    $("#description-2").toggle(500)
+})
+
+$("#plus3").click(function() {
+  
+    $("#description-3").toggle(600)
+})
+/////start index2
+
+$("#icon").click(function() {
+  
+    $("#Brief").toggle(500)
+})
+
+$("#icon2").click(function() {
+  
+    $("#Brief-2").toggle(1000)
+})
+
+$("#icon3").click(function() {
+  
+    $("#Brief-3").toggle(500)
+})
+
